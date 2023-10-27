ed0e06ce9203509fbf42300ed2ee13be462d6674
Sachidananda S V <sachivips@gmail.com>
2023-10-27 11:42:33 +0530

    Initial Commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(noteItem){
+    return <Note 
+    key={noteItem.key}
+    title={noteItem.title}
+    content={noteItem.content}
+    />
+}
+
+function App(){
+    return(<div>
+        <Header />
+        {notes.map(createNotes)}
+        <Footer />
+    </div>)         
+}
+
+export default App;
